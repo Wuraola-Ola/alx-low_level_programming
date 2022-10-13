07b763c26a8cb94f0699ca308cb49a14eb75a8fc
Wuraola-Ola <wuraolawaiye@gmail.com>
2022-10-13 14:16:06 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  *  * main - entry point
@@ -12,51 +13,11 @@
  *      */
 
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-#include <stdio.h>
-
-/**
- *
- *  * main - entry point
- *
- *   * @void: no arguments
- *
- *    * Description: the program starts and ends here.
- *
- *     * Return: value 0 to exit
- *
- *      */
-
-int main(void)
-
-{
-
-puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
-
-#include <stdio.h>
-
-/**
- *
- *  * main - entry point
- *
- *   * @void: no arguments
- *
- *    * Description: the program starts and ends here.
- *
- *     * Return: value 0 to exit
- *
- *      */
-
-int main(void)
-
-{
-
-puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
 
 }
