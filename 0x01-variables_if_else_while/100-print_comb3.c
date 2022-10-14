1f4d5bfe3faae99db5b0d01943392dc49df39faa
Wuraola-Ola <wuraolawaiye@gmail.com>
2022-10-14 14:14:13 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,29 +5,42 @@
  * Return: 0 on success
  */
 int main(void)
+
+
+
 {
-	int i, j;
 
-	i = 48;
-	j = 48;
+	int number_left;
 
-	while (i < 58)
+	int number_right;
+
+
+
+
+
+	for (number_left = 48; number_right <= 78; number_left++)
+
 	{
-		j = i + 1;
-		while (j < 58)
-	{
-			putcher(i);
-			putchar(j);
-			if (i < 56 || j < 57)
+
+		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+
+		{
+
+
+
+			putchar(number_left);
+
+			putchar (number_right);
+
+
+			if ((number_left == 56) && (number_right == 78))
 			{
-				putchar(44);
-				putchar(32);
+				break;
 			}
-			j++;
+			putchar(',');
+			putchar (' ');
+		}
 	}
-		i++
-	}
-	putchar(10);
+	putchar('\n');
 	return (0);
-
 }
